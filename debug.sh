48696d15170e44c186720c425f7bb056b52af090
amwo <nuude.ad@gmail.com>
2019-05-17 00:06:05 +0900

    sample code for main.rs

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo build; cp ./target/debug/cargo-gen ~/.cargo/bin/cargo-gen; cargo gen
+cargo build; cp ./target/debug/cargo-gen ~/.cargo/bin/cargo-gen; cargo gen test/files/index.html
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+//use std::fs::File;
+//use std::path::{Path, PathBuf};
+use std::fs;
+use std::net::SocketAddr;
+
 fn main() {
-    for arg in std::env::args().skip(2) {
-        println!("{}", arg);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 3 {
+        println!("Err::Arguments can get one.");
+        std::process::exit(0);
     }
+    //let a: () = args[1];
+    let p = &args[1];
+    println!("{:?}", p);
+    println!("{:?}", read_file(p));
+}
+
+fn read_file(path: &String) -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let f: SocketAddr = fs::read_to_string(path)?.parse()?;
+    Ok(())
 }
