36e61018f998c68904ff1cd6b57e75610feb52d3
amwo <nuude.ad@gmail.com>
2019-05-26 23:49:40 +0900

    Add file read file write

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo build; cp ./target/debug/cargo-gen ~/.cargo/bin/cargo-gen; cargo gen test/files/index.html
+cargo build; cp ./target/debug/cargo-gen ~/.cargo/bin/cargo-gen; cargo gen tests/files/index.html;
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
-//use std::net::SocketAddr;
+use std::env;
 use std::fs;
+use std::io::{BufWriter, Write};
 use std::path::Path;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() == 3 {
+
+    if args.len() >= 4 {
         println!("Err::Arguments can get one.");
         std::process::exit(0);
     }
 
-    let p = Path::new(&args[1]); //&std::path::Path
-    let html = read_file(p);
-    println!("{:?}", html.unwrap());
+    let p = Path::new(&args[2]); //&std::path::Path
+    let html = read_file(&p);
+    println!("{:?}", html);
+
+    //println!("{:?}", html.unwrap());
+
+    write();
+}
+
+fn write() {
+    let b = b"test";
+    let mut f = BufWriter::new(fs::File::create("test.html").unwrap());
+    for _ in 0..4 {
+        f.write(b).unwrap();
+    }
 }
 
 fn read_file(p: &std::path::Path) -> Result<String, Box<dyn std::error::Error + 'static>> {
+    let current_dir = env::current_dir()?;
+    let p = current_dir.join(p);
+    println!("{:?}", p);
     let html = fs::read_to_string(p)?;
     Ok(html)
 }
+
 //:RustRun test/files/index.html
